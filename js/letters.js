01f210e071bd8431b0e9471ac7574fb4008af05c
JeremyEssig <jeremy.essig@gmail.com>
2022-01-15 16:00:42 +0100

    Titre machine à écrire corrigé

--- js/letters.js
+++ js/letters.js
@@ -1,40 +1,56 @@
-const target = document.getElementById('animated-text-target');
-let array=["Bonjour,", "Hi,", "Hallo,", "Grüzi"];
-let wordIndex = 0;
-let letterIndex =0;
-
-const createLetter = () =>{
-    const letter = document.createElement('span');
-    target.appendChild(letter);
-
-    letter.textContent = array[wordIndex][letterIndex];
-
-    setTimeout(()=>{
-        letter.remove();
-    }, 2800);
-
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
 };
 
-//-----------------------------------------------
-// Fonction récursive
-const loop = ()=>{
-    setTimeout(()=>{
-        if(wordIndex >= array.length){
-            wordIndex = 0;
-            letterIndex = 0;
-            loop();
-        }else if(letterIndex < array[wordIndex].length){
-            createLetter();
-            letterIndex++;
-            loop();
-        }else{
-            wordIndex++;
-            letterIndex = 0;
-            setTimeout(()=> {
-                loop();
-            }, 2800);
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="typewriter-wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
         }
-    }, 60);
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .typewriter-wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
 };
-
-loop()
